0793399f06fea93e45a7aef5d1f7d784f7382f07
Dmytro Kovtun <dmytro.kovtun@guesty.com>
2022-12-11 12:52:09 +0200

    Integrate AppEngine

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const PORT = Number(process.env.PORT) || 8080;
+  await app.listen(PORT);
 }
 bootstrap();
