37a54a88f0f3c3efef718ec8f51b24ec3443f823
Dmytro Kovtun <dmytro.kovtun@guesty.com>
2022-12-23 10:25:42 +0200

    Update bot commands

--- src/telegram-bot/bot.service.ts
+++ src/telegram-bot/bot.service.ts
@@ -3,6 +3,15 @@ import { Bot } from 'grammy';
 import { DatabaseService } from '../db/database.service';
 import { HttpService } from '@nestjs/axios';
 
+const hayThere = `
+<bold>Hi there! You are subscribed to the lights switches on Lomonosova 71z</bold>
+type <span>/unsubscribe</span> to leave
+`;
+
+const bayText = `
+You won't receive notifications. You can always type <span>/start</span> to re resubscribe
+`;
+
 @Global()
 @Injectable()
 export class BotService {
@@ -19,11 +28,15 @@ export class BotService {
   async sendPush(status: 'ON' | 'OFF') {
     const { botToken } = require('../../credentials/telegram.json');
     const subscribersQuery = await this.dbService.db.collection(this.tableName).get();
+    const message = status === 'ON' ?
+      `💡🟨️Yay! Ми знову зі світлом! ⚡` :
+      `🌚⬛️От халепа! \n Світла знову нема`;
+
     subscribersQuery.forEach(subscriber => {
       const chatId = subscriber.get('chatId');
       this.httpService.post(`https://api.telegram.org/bot${botToken}/sendMessage`, {
         'chat_id': chatId,
-        'text': 'Light is ' + status,
+        'text': message,
         'parse_mode': 'HTML'
       }).subscribe();
     });
@@ -54,17 +67,36 @@ export class BotService {
     return await this.dbService.db.collection(this.tableName).add({ chatId });
   }
 
+  private async unsubscribeUser(chatId: number) {
+    const doc = await this.dbService.db
+      .collection(this.tableName)
+      .where('chatId', '==', chatId)
+      .get();
+    return doc.forEach(doc => doc.ref.delete());
+  }
+
   private addListeners() {
     console.log('addListeners');
     this.bot.command('start', async (ctx) => {
       const chatId = ctx.update.message.chat.id;
-      // console.log(ctx);
-      // console.log(ctx.update.message.chat);
       const subscribed = await this.isUserSubscribed(chatId);
+
       if (!subscribed) {
         await this.subscribeUser(chatId);
       }
-      await ctx.reply('test', {
+      await ctx.reply(hayThere, {
+        parse_mode: 'HTML'
+      });
+    });
+
+    this.bot.command('unsubscribe', async (ctx) => {
+      const chatId = ctx.update.message.chat.id;
+      const subscribed = await this.isUserSubscribed(chatId);
+
+      if (subscribed) {
+        await this.unsubscribeUser(chatId);
+      }
+      await ctx.reply(bayText, {
         parse_mode: 'HTML'
       });
     });
